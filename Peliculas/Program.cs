de89dcf3811b2fd07e8f7c06e28a73c39d2ca648
JonathanMov <jafetmv21@gmail.com>
2020-02-19 17:56:22 -0800

    primera versión

--- Peliculas/Program.cs
+++ Peliculas/Program.cs
@@ -7,9 +7,20 @@
     }
     class Program{
         static void Main(){
-            Pelicula.titulo="Interestelar";
-            Pelicula.año=2014;
-            Pelicula.pais="Estados Unidos";
-            Pelicula.director="Christopher Nolan";
+            Pelicula p1=new Pelicula();
+            p1.titulo="La forma del agua";
+            p1.año=2017;
+            p1.pais="Estados Unidos";
+            p1.director="Guillermo del Toro";
+
+            Console.WriteLine("La película {0} se estrenó el año {1}.",p1.titulo,p1.año);
+
+            Pelicula p3=new Pelicula();
+            p3.titulo="El discurso del Rey";
+            p3.año=2010;
+            p3.pais="Reino Unido";
+            p3.director="Tom Hooper";
+
+            Console.WriteLine("La película {0} se estrenó el año {1}.",p3.titulo,p3.año);
         }
     }
