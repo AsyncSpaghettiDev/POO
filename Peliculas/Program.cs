44dd41b13ed2cf9caa1caa363fa0ab99537f4e64
JonathanMov <jafetmv21@gmail.com>
2020-02-19 17:42:12 -0800

    Paso 1

---
+++ Peliculas/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+    class Pelicula{
+        public string titulo;
+        public Int16 año;
+        public string pais;
+        public string director;
+    }
+    class Program{
+        static void Main(){
+            Pelicula p1 = new Pelicula();
+            p1.titulo="Interestelar";
+            p1.año=2014;
+            p1.pais="Estados Unidos";
+            p1.director="Christopher Nolan";
+        }
+    }
