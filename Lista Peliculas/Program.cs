4980b0e6027e71a80a0b9b6ea658bcb1887a38a5
JonathanMov <60447812+JonathanMov@users.noreply.github.com>
2020-02-21 12:32:57 -0800

    Actividad 2 Lista de Peliculas

---
+++ Lista Peliculas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Pelicula{
+        private string titulo;
+        public Pelicula(string titulo){
+            this.titulo=titulo;
+        }
+        public void imprime(){
+            Console.WriteLine(titulo);
+        }
+    }
+    class Program{
+        static void Main(){
+            List<Pelicula> peliculas= new List<Pelicula>();
+            peliculas.Add(new Pelicula("El Rey Leon"));
+            peliculas.Add(new Pelicula("Jurasic Park"));
+            peliculas.Add(new Pelicula("Titanic"));
+            peliculas.Add(new Pelicula("Interstellar"));
+            peliculas.Add(new Pelicula("The Revenant"));
+
+            foreach (Pelicula a in peliculas){
+                a.imprime();
+            }
+        }
+    }
+
