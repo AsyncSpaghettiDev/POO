d557f3ca596b9cc693706c56ede693e5cd3921be
JonathanMov <60447812+JonathanMov@users.noreply.github.com>
2020-03-27 20:36:25 -0700

    Creacion de proyecto Duracion

---
+++ Duracion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Duracion{
+    private int horas;
+    private int minutos;
+    private int segundos;
+    public Duracion(int horas,int minutos,int segundos){
+        this.horas=horas;
+        this.minutos=minutos;
+        this.segundos=segundos;
+    }
+    public Duracion(int min){
+        if(min>59){
+            for(int i=min;i>59;i-=60){
+                int horas=minutos/i;
+                this.horas=horas;
+                this.minutos=i;
+            }
+        }
+        this.segundos=0;
+    }
+    public override string ToString()=>string.Format("{0}:{1}:{2}");
+}
+class Program{
+    static void Main(){
+        Duracion a=new Duracion(60);
+        Console.WriteLine(a);
+    }
+}
