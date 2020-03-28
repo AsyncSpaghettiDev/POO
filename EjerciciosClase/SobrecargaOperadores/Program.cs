e754fe8664986ec49c6e2f3ecc036aabeafcbe75
JonathanMov <60447812+JonathanMov@users.noreply.github.com>
2020-03-27 18:02:47 -0700

    Agregar metodo simplificar

--- EjerciciosClase/SobrecargaOperadores/Program.cs
+++ EjerciciosClase/SobrecargaOperadores/Program.cs
@@ -8,7 +8,20 @@ class Racional{
     }
     public int getNumerador()=>numerador;
     public int getDenominador()=>denominador;
-    public override string ToString()=> String.Format("{0}/{1}",numerador,denominador);
+    public void simplificar(){
+        for(int i=1;i<9;i++){
+            int num=numerador%i;
+            int den=denominador%i;
+            if((num==0)&(den==0)){
+                this.numerador=numerador/i;
+                this.denominador=denominador/i;
+            }
+        }
+    }
+    public override string ToString(){
+        if(denominador==1)return String.Format("{0}",numerador);
+        else return String.Format("{0}/{1}",numerador,denominador);
+    }
     public static Racional operator +(Racional x, Racional y){
         int numerador=(x.getNumerador()*y.getDenominador())+(y.getNumerador()*x.getDenominador());
         int denominador=x.getDenominador()*y.getDenominador();
@@ -20,7 +33,9 @@ class Program{
         Racional a=new Racional(2,3);
         Racional b=new Racional(3,4);
         Racional c=a+b;
+        Racional d=new Racional(30,8);
+        d.simplificar();
 
-        Console.WriteLine("{0}\n{1}\n{2}",a,b,c);
+        Console.WriteLine("{0}\n{1}\n{2}\nSimplificado:{3} ",a,b,c,d);
     }
 }
