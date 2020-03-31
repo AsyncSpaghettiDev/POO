eb63faf5ce53c89250062fc1927daa7e6f8f3b97
JonathanMov <60447812+JonathanMov@users.noreply.github.com>
2020-03-31 13:14:32 -0700

    Creación primera clase

--- Alumnos/Program.cs
+++ Alumnos/Program.cs
@@ -1,4 +1,15 @@
 ﻿using System;
+class Alumno{
+    protected string nombre;
+    protected int matricula;
+    public Alumno(string nombre,int matricula){
+        this.nombre=nombre;
+        this.matricula=matricula;
+    }
+    public virtual void imprimeGrado(){
+        Console.WriteLine("Mi nombre es {}");
+    }
+}
 class Program{
     static void Main(){
         Console.WriteLine("Hello World!");
