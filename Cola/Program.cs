bc74d78dfebcb4e18219a4e69810c78591040a39
JonathanMov <60447812+JonathanMov@users.noreply.github.com>
2020-05-11 23:56:58 -0700

    Documentacion

--- Cola/Program.cs
+++ Cola/Program.cs
@@ -1,30 +1,41 @@
 ﻿using System;
+/*Creacion de clase generica tupo cola FirstInput FirstOutput*/
 class Cola<T>{
     private readonly int capacidad;
     private int apuntador=0;
     private T[] miembros;
+    /*Sobrecarga de constructor*/
     public Cola():this(8){}
+    /*Se declara el tamaño de la estructura y se inicializa*/
     public Cola(int capacidad){
         this.capacidad=capacidad;
         this.miembros= new T[this.capacidad];
     }
+    /*Se añade un nuevo miembro a la estructura antes validando que el apuntador no exceda la capacidad de
+    *la estructura, de ser asi se lanza una excepcion
+    */
     public void Push(T miembro){
-        if(this.apuntador>=0 && this.apuntador<this.capacidad){
-            miembros[this.apuntador]= miembro;
-            apuntador++;
-        }
-        else
+        if(this.apuntador>=this.capacidad)
             throw new Exception("No se pueden agregar más elementos porque la fila esta llena");
+        else{
+            miembros[this.apuntador]= miembro;
+            this.apuntador++;
+        }
+            
     }
+    /*Se guarda el valor de la primer posicion en una variable temporal y se recorre todo la
+    *estructura -1 para dejar el ultimo espacio vacio, disponible para ser ocupado.
+    */
     public T Pop(){
         this.apuntador--;
         if(this.apuntador>=0 && this.apuntador<=this.capacidad){
-            T temp= miembros[0];
+            var temp= miembros[0];
             for(int i=0;i<this.apuntador;i++)
                 miembros[i]=miembros[i+1];
             return temp;
         }
-        throw new Exception("No se pueden sacar más elementos porque la fila esta vacia");
+        else
+            throw new Exception("No se pueden sacar elementos porque la fila esta vacia");
     }
 }
 class Program{
