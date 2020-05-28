bf1a833f22d2db6405253996189d267e7a1de71c
JonathanMov <60447812+JonathanMov@users.noreply.github.com>
2020-05-27 23:12:44 -0700

    Implementacion de la interfaz IHeader en Producto

---
+++ Productos/Abstractas_Interfaces.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Productos {
+    interface IHeader {
+        string txt_Header();
+    }
+    /// <summary>
+    /// Definicion de producto, no se puede instanciar.
+    /// </summary>
+    abstract class Producto :IHeader{
+        public int departamento { get; set; }
+        public string codigo {
+            get => this.codigo;
+            set {
+                this.codigo = String.Format("{0}-{1}", this.departamento, value.Replace(' ', '-'));
+            }
+        }
+        public string descripcion { get; set; }
+        //List<PrecioFecha> precios;
+        public int likes { get; set; }
+        public int precio {
+            set {
+                this.precio = value;
+            }
+            /*get {
+                this.precios.PrecioActual;
+            }*/
+        }
+        /*Sobrecarga de constructor*/
+        public Producto() { }
+        public Producto(int departamento, string codigo, string descripcion, int likes, int precioBase) {
+            this.departamento = departamento;
+            this.codigo = codigo;
+            this.descripcion = descripcion;
+            this.likes = likes;
+            this.precio = precioBase;
+        }
+        string IHeader.txt_Header() {
+            return String.Format("Departamento|Codigo|Descripcion|Likes|Precio|Fecha_Registro");
+        }
+    }
+}
--- Productos/Producto.cs
+++ Productos/Producto.cs
@@ -3,35 +3,15 @@ using System.Collections.Generic;
 
 namespace Productos {
     /// <summary>
-    /// Definicion de producto, no se puede instanciar.
-    /// </summary>
-    abstract class Producto {
-        public int departamento { get; set; }
-        public string codigo {
-            get => this.codigo;
-            set {
-                this.codigo = String.Format("{0}-{1}",this.departamento,value.Replace(' ','-'));
-            }
-        }
-        public string descripcion { get; set; }
-        //List<PrecioFecha> precios;
-        public int likes { get; set; }
-        /*Sobrecarga de constructor*/
-        public Producto() { }
-        public Producto(int departamento, string codigo,string descripcion,int likes) {
-            this.departamento = departamento;
-            this.codigo = codigo;
-            this.descripcion = descripcion;
-            this.likes = likes;
-        }
-    }
-    /// <summary>
     /// Definicion de mercancía que tiene una caducidad, que se echa a perder, y que se le debe dar la debida rotación antes de que se deteriore y se haga no apta para la venta, o bien, que se convierta en merma.
     /// </summary>
     class ProductoPerecedero : Producto {
-        public ProductoPerecedero(int departamento, string codigo, string descripcion, int likes) : base(departamento,codigo,descripcion,likes) { }
+        public ProductoPerecedero(int departamento, string codigo, string descripcion, int likes, int precioBase) : base(departamento,codigo,descripcion,likes, precioBase) { }
     }
+    /// <summary>
+    /// Definicion de aquellos artículos que son clasificados como no básicos,
+    /// </summary>
     class ProductoNoPerecedero : Producto {
-        public ProductoNoPerecedero(int departamento, string codigo, string descripcion, int likes) : base(departamento, codigo, descripcion, likes) { }
+        public ProductoNoPerecedero(int departamento, string codigo, string descripcion, int likes, int precioBase) : base(departamento, codigo, descripcion, likes, precioBase) { }
     }
 }
