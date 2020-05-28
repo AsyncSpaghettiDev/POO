66a0a680d45ff426a200f0a73de4d2669a574b1c
JonathanMov <60447812+JonathanMov@users.noreply.github.com>
2020-05-27 22:11:29 -0700

    Integracion de botones de control

--- Productos/Inventario.cs
+++ Productos/Inventario.cs
@@ -4,6 +4,7 @@ namespace Productos {
     partial class Inventario : Form {
         public Inventario() {
             initializeComponent();
+            Funciones.Diseno(this,500,500,"Control de Inventarios","ILogo");
         }
     }
 }
