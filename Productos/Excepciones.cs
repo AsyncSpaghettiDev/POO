dec7e8578503903b02e06bf39eaf2ce31bddcef0
JonathanMov <60447812+JonathanMov@users.noreply.github.com>
2020-05-27 22:50:48 -0700

    Implementacion de clase producto y derivadas

---
+++ Productos/Excepciones.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace Productos {
+    class InvalidDepartamentException : Exception {
+        public InvalidDepartamentException() : base("Departamento Inválido revisa la tabla de Departamentos") { }
+    }
+}
